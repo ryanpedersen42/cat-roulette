cf02674109cae32f7ec93be2ecddfc51b1c3bbf1
Ryan Pedersen <ryanpedersen@Ryans-MBP-2.attlocal.net>
2020-03-20 08:51:52 -0500

    add test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,21 +1,11 @@
 import Web3 from "web3";
 import PrivateKeyProvider from "truffle-privatekey-provider";
 
-describe("Auths", () => {
-  it("I can increment number of clicks", () => {
-    cy.on("window:before:load", (win) => {
-      const provider = new PrivateKeyProvider(
-        "insert private key...",
-        "https://rinkeby.infura.io/"
-      );
-      win.web3 = new Web3(provider);
-    });
-
-
-
+describe("Loads", () => {
+  it("starts loading process", () => {
     cy.visit("http://localhost:3000");
 
-    cy.wait(10000);
+    cy.contains('Hold tight');
 
   });
 });
